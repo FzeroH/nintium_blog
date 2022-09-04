43af6b7ce48ae654735a264db72bbeee2f27cb92
FzeroH <vampir666x@gmail.com>
2022-09-04 23:29:02 +0300

    Добавил страницу dashboard

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="menu-container">
+    <nav>
+      <router-link to="/" class="logo">
+        <img src="@/assets/images/dashboard-nav/dashboard-logo.svg" alt="logo">
+      </router-link>
+      <ul>
+        <li>
+          <img src="@/assets/images/dashboard-nav/grid.svg" alt="dashboard">
+          <span>Dashboard</span>
+        </li>
+        <li>
+          <img src="@/assets/images/dashboard-nav/user.svg" alt="profile">
+          <span>Profile</span>
+        </li>
+        <li>
+          <img src="@/assets/images/dashboard-nav/edit.svg" alt="write a post">
+          <span>Write a Post</span>
+        </li>
+        <li>
+          <img src="@/assets/images/dashboard-nav/book-open.svg" alt="all posts">
+          <span>All posts</span>
+        </li>
+        <li>
+          <img src="@/assets/images/dashboard-nav/box.svg" alt="resources">
+          <span>Resources</span>
+        </li>
+        <li>
+          <img src="@/assets/images/dashboard-nav/settings.svg" alt="settings">
+          <span>Settings</span>
+        </li>
+        <li>
+          <img src="@/assets/images/dashboard-nav/help-circle.svg" alt="help">
+          <span>Help</span>
+        </li>
+      </ul>
+    </nav>
+    <div class="dashboard">
+      <div class="dashboard-title">
+        <h2>Dashboard</h2>
+        <button>Customize</button>
+      </div>
+      <div class="stat-cards">
+        <stat-cards
+          :card-name="'Total Posts'"
+          :image="'total-posts'"
+          :count="'214K'"/>
+
+        <stat-cards
+          :card-name="'Total Views'"
+          :image="'total-views'"
+          :count="'113K'"/>
+
+        <stat-cards
+          :card-name="'Most Viewed Article'"
+          :count="'The Most Awesome Article Man Has Ever Written'"/>
+      </div>
+      <h2>Total Views</h2>
+      <Line :width="400" :height="400"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import StatCards from '@/components/StatCards.vue';
+import { Line } from 'vue-chartjs/legacy';
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Dashboard',
+  components: { StatCards, Line },
+
+};
+</script>
+
+<style lang="scss" scoped>
+  .menu-container {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 100%;
+
+    nav {
+      background: #3B3F44;
+      width: 14rem;
+      height: 100vh;
+
+      .logo {
+        width: 11rem;
+        height: 3.6rem;
+        margin: 2rem 1.6rem 3rem 1.6rem;
+      }
+
+      ul {
+        li {
+          display: flex;
+          align-items: center;
+
+          &:first-child {
+            margin-top: 3.2rem;
+          }
+
+          &:not(:first-child) {
+            margin-top: 2.1rem;
+          }
+
+          &:hover {
+            background: #545A61;
+          }
+
+          span {
+            font-family: 'Open Sans',serif;
+            font-style: normal;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 22px;
+            color: white;
+            margin-left: 0.9rem;
+          }
+
+          img {
+            width: 30px;
+            height: 30px;
+          }
+        }
+      }
+    }
+
+    .dashboard {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: fit-content;
+
+      .dashboard-title {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin-top: 107px;
+
+        h2 {
+          font-family: 'Libre Baskerville', serif;
+          font-style: normal;
+          font-weight: 400;
+          font-size: 26px;
+          line-height: 32px;
+          margin: 1.2rem 2.6rem;
+        }
+
+        button {
+          border: 2px solid #1C1C1C;
+          border-radius: 5px;
+          padding: 0.5rem 0.6rem;
+          width: 7rem;
+          height: 2.6rem;
+          margin: 1.2rem 3rem;
+          font-family: 'Open Sans', serif;
+          font-style: normal;
+          font-weight: 400;
+          font-size: 16px;
+          line-height: 22px;
+        }
+
+      }
+
+      .stat-cards {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+      }
+
+      h2 {
+        font-family: 'Libre Baskerville',serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 26px;
+        line-height: 32px;
+        margin: 2.5rem 0 0 2rem;
+      }
+
+    }
+  }
+</style>
---
+++ src/components/StatCards.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="cards-container">
+    <p>{{ cardName }}</p>
+    <div class="count-container">
+      <img :src="require(`../assets/images/${ image }.svg`)" alt="card" v-if="image.length !== 0">
+      <p class="count"
+         :class="{ 'most-viewed-article': cardName === 'Most Viewed Article' }">
+        {{ count }}
+      </p>
+    </div>
+    <span>This month</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StatCards',
+  props: {
+    cardName: {
+      type: String,
+      default: '',
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    count: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.cards-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: 0.125rem solid rgba(28, 28, 28, 0.35);
+  border-radius: 0.6rem;
+  width: fit-content;
+  height: 6.7rem;
+  margin: 0 1.5rem;
+
+  p {
+    align-self: start;
+    margin: 0.6rem;
+  }
+  span {
+    font-family: 'Roboto',serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    color: rgba(28, 28, 28, 0.5);
+    margin-bottom: 0.9rem;
+    margin-right: 0.5rem;
+    align-self: end;
+  }
+}
+
+.count-container {
+  display: flex;
+  align-self: center;
+  flex-direction: row;
+  align-items: center;
+  margin: 0 auto;
+
+  img {
+    width: 1.7rem;
+    height: 1.7rem;
+    margin-right: 0.6rem;
+    margin-left: 0.9rem;
+  }
+
+  .count {
+    font-family: 'Libre Baskerville',serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 33px;
+    line-height: 41px;
+  }
+
+  .most-viewed-article {
+    font-family: 'Libre Baskerville',serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 22px;
+    line-height: 27px;
+  }
+
+}
+</style>
