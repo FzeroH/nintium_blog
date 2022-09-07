0ae5597e335b79ad0f00710496977b18cff6e7e1
FzeroH <vampir666x@gmail.com>
2022-09-07 13:08:14 +0300

    Исправил отображение хедера и добавил анимацию строке поиска

--- src/components/NintiumHeader.vue
+++ src/components/NintiumHeader.vue
@@ -1,32 +1,39 @@
 <template>
   <header :class="{ 'screenlock-header': $route.path === '/screenlock'}"
           v-if="$route.path !== '/dashboard'">
-    <router-link to="/" class="logo" v-if="$route.path !== '/dashboard'">
-      <img src="@/assets/images/logo.svg" alt="logo" class="logo">
-    </router-link>
-    <!--TODO: Доделать header для всех страниц-->
-    <router-link to="/"
-                 :class="{ active: $route.path  === '/'}"
-                 v-if="$route.path !== '/screenlock'">
-      Home
-    </router-link>
-    <router-link to="/tags"
-                 :class="{ active: $route.path === '/tags'}"
-                 v-if="$route.path !== '/screenlock'">
-      Tags
-    </router-link>
-    <router-link to="/about"
-                 :class="{ active: $route.path === '/about'}"
-                 v-if="$route.path !== '/screenlock'">
-      About
-    </router-link>
-    <div class="search-container" v-if="$route.path !== '/screenlock'">
-          <!--TODO: Добавить скрывашку и раскрывашку для поиска! Создать div position relative. -->
-    <!-- eslint-disable-next-line -->
-      <input type="text" id="search"/>
-      <img src="@/assets/images/search.svg" alt="search" class="search-img">
+    <div class="header-section">
+      <router-link to="/" class="logo" v-if="$route.path !== '/dashboard'">
+        <img src="@/assets/images/logo.svg" alt="logo" class="logo">
+      </router-link>
+      <!--TODO: Доделать header для всех страниц-->
+      <router-link to="/"
+                   :class="{ active: $route.path  === '/'}"
+                   v-if="$route.path !== '/screenlock'">
+        Home
+      </router-link>
+      <router-link to="/tags"
+                   :class="{ active: $route.path === '/tags'}"
+                   v-if="$route.path !== '/screenlock'">
+        Tags
+      </router-link>
+      <router-link to="/about"
+                   :class="{ active: $route.path === '/about'}"
+                   v-if="$route.path !== '/screenlock'">
+        About
+      </router-link>
     </div>
-    <router-link to="/login" class="login" v-if="$route.path !== '/screenlock'">Login</router-link>
+    <div class="header-section">
+      <div class="search-container" v-if="$route.path !== '/screenlock'">
+        <!-- eslint-disable-next-line -->
+        <input type="text" id="search"/>
+        <button type="submit">
+          <img src="@/assets/images/search.svg" alt="search"/>
+        </button>
+      </div>
+      <router-link to="/login" class="login" v-if="$route.path !== '/screenlock'">
+        Login
+      </router-link>
+    </div>
   </header>
 </template>
 
@@ -51,26 +58,31 @@ header {
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 
-  .logo {
-    width: 178px;
-    height: 57px;
-    margin-top: 31px;
-    margin-left: 25px;
+  .header-section {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    position: relative;
 
-    img {
-      margin: 0;
+    .logo {
+      width: 178px;
+      height: 57px;
+      margin-top: 31px;
+      margin-left: 25px;
+
+      img {
+        margin: 0;
+      }
     }
-  }
 
-  a {
-    font-weight: 400;
-    font-size: 20px;
-    color: black;
-    margin: 49px 0 0 31px;
-    text-align: center;
-    text-decoration: none;
+    a {
+      font-size: 20px;
+      color: black;
+      margin: 49px 0 0 31px;
+      text-align: center;
+    }
   }
 }
 
@@ -89,25 +101,73 @@ header {
 }
 
 .search-container {
+  display: flex;
+  flex-direction: row;
   position: relative;
-  width: 21.4rem;
+  width: fit-content;
   margin-top: 35px;
-  margin-left: 344px;
+  border: none;
+  border-radius: 10px;
+  transition: linear;
 
-  input {
-    outline: none;
-    width: 100%;
-    height: 46px;
-    font-size: 20px;
+  &:hover {
     border: solid 2px #1C1C1C;
-    border-radius: 10px;
-    padding-right: 37px;
+    width: 21.4rem;
   }
 
-  .search-img {
+  &:focus {
+    border: solid 2px #1C1C1C;
+    width: 21.4rem;
+  }
+
+  &:hover input {
+    width: 100%;
+    right: 0;
+    transform: translateX(0);
+  }
+
+  &:hover button {
     position: absolute;
-    left: 351px;
-    top: 11px;
+    right: 0;
+    transform: translateX(0);
+  }
+
+  input {
+    position: relative;
+    right: -100%;
+    outline: none;
+    border: none;
+    width: 1.5rem;
+    height: 46px;
+    border-radius: 10px;
+    font-size: 20px;
+    padding-left: 0.5rem;
+    //background: orangered;
+    transform: translateX(-100%);
+    transition: 0.2s linear;
+
+    &:focus {
+      width: 100%;
+      right: 0;
+      transform: translateX(0);
+      & + button {
+        position: static;
+        transform: translateX(0);
+      }
+    }
+
+  }
+
+  button {
+    position: absolute;
+    width: 30px;
+    height: 30px;
+    background: none;
+    border: none;
+    margin-top: 0.5rem;
+    margin-right: 0.8rem;
+    top: 0;
+    right: 0;
   }
 }
 
@@ -119,10 +179,9 @@ header {
   color: black;
   width: 135px;
   height: 46px;
-  margin: 35px 0 0 50px;
+  margin: 35px 25px 0 9px !important;
   border: solid 2px #1C1C1C;
   border-radius: 10px;
-  font-weight: 400;
   font-size: 20px;
 }
 </style>
