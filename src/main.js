3ca8b368fdd98e7a08be3a13c32e3cdddef5b4a2
FzeroH <vampir666x@gmail.com>
2022-10-19 22:34:54 +0300

    Добавил vuex

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import '@/assets/styles.scss';
+// eslint-disable-next-line import/order
+import store from '@/store';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount('#app');
